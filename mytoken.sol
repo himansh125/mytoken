22a7751f078f3bfbbef8c8eac36bc1d38ca9ceb9
himansh125 <149005507+himansh125@users.noreply.github.com>
2023-10-31 20:33:29 +0530

    Create mytoken.sol

---
+++ mytoken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+contract MyToken {
+    // public variables here
+    string public tokenname = "lithium"; 
+    string public tokenAbbrv = "lth"; 
+    uint public totalSupply = 0;
+
+    // mapping variable here 
+    mapping(address => uint) public balances;
+
+    // mint function
+    function mint (address _to , uint _value) public {
+        totalSupply += _value; 
+        balances[_to] += _value;
+    }
+    // burn function
+    function burn(address _from, uint _value) public {
+        require(balances[_from] >= _value, "Insufficient balance");
+        totalSupply -= _value;
+        balances[_from] -= _value;
+        }
+}
+
